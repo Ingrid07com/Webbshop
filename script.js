3b2bdd2e01df7d26ca727c339a4a108c390e79d5
Ingrid Gistorp <ingrid.gistorp@elev.ga.ntig.se>
2025-03-12 08:55:17 +0100

    Försöker lösa varför Cyclope inte kan läggas till i varukorgen

--- script.js
+++ script.js
@@ -3,44 +3,50 @@ let cart = [];
 
 // Funktion för att lägga till  produkter till varukorgen
 function addToCart(name, price) {
-    cart.push({ name, price });
-    updateCart();
+  cart.push({ name, price });
+  updateCart();
 }
 
 // Funktion för att uppdatera varukorg displayen
 function updateCart() {
-    let cartList = document.querySelector("ul");
-    cartList.innerHTML = "";
-    let total = 0;
+  let cartList = document.querySelector("ul");
+  cartList.innerHTML = "";
+  let total = 0;
 
-    cart.forEach((item, index) => {
-        total += item.price;
-        cartList.innerHTML += `
+  cart.forEach((item, index) => {
+    total += item.price;
+    cartList.innerHTML += `
         <li class="VarukorgText" >${item.name} - ${item.price} kr 
         <button class="RemoveKnappar" onclick="removeFromCart(${index})">Remove</button>
         </li>`;
-    });
+  });
 
-    cartList.innerHTML += `<li><strong>Total: ${total} kr</strong></li>`;
+  cartList.innerHTML += `<li><strong>Total: ${total} kr</strong></li>`;
 }
 
 // Funktion för att ta bort produkter från varukorgen
 function removeFromCart(index) {
-    cart.splice(index, 1);
-    updateCart();
+  cart.splice(index, 1);
+  updateCart();
 }
 
-// Sätt ihop listeners till "Add to cart" knapparna 
+// Sätt ihop listeners till "Add to cart" knapparna
 document.addEventListener("DOMContentLoaded", () => {
-    let buttons = document.querySelectorAll("button");
+  let buttons = document.querySelectorAll("button");
 
-    buttons.forEach((button) => {
-        button.addEventListener("click", (event) => {
-            let parent = event.target.closest("div");
-            let name = parent.querySelector("h2").innerText;
-            let price = parseInt(parent.querySelector("p").innerText.replace("Pris: ", "").replace("kr", "").replace(/\s+/g, ''));
+  buttons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      let parent = event.target.closest("div");
+      let name = parent.querySelector("h2").innerText;
+      let price = parseInt(
+        parent
+          .querySelector("p")
+          .innerText.replace("Pris: ", "")
+          .replace("kr", "")
+          .replace(/\s+/g, "")
+      );
 
-            addToCart(name, price);
-        });
+      addToCart(name, price);
     });
+  });
 });
